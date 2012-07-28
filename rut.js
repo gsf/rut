df7b0efdd7e721b4a5d127c331d0c4ad15f362fd
Gabriel Farrell <g@grrawr.com>
2012-07-28 15:44:06 -0400

    Added method handling and route caching

--- rut.js
+++ rut.js
@@ -1,27 +1,45 @@
 var url = require('url')
 
-// special characters that need to be escaped when passed to `RegExp()`, lest
-// they be interpreted as pattern-matching:
-var specialChars = /[|.+?{}()\[\]\^$]/g;
+// special characters that need to be escaped when passed to `RegExp()` 
+// lest they be interpreted as pattern-matching
+var specialChars = /[|.+?{}()\[\]\^$]/g
 
-function rut(route, cb) {
+// cache for route RegExps
+var cache = {}
+
+function rut(route, options, cb) {
+  if (!cb) {
+    cb = options
+    options = {}
+  }
+  // convert route to RegExp with matching * groups if not already a RegExp
+  // use constructor because instanceof fails between modules
+  if (route.constructor.name != 'RegExp') {
+    route = cache[route] || (cache[route] = new RegExp('^' + String(route)
+      .replace(specialChars, '\\$&')
+      .replace(/\*\*/g, '(.+)')
+      .replace(/\*/g, '([^/]+)') + '$'
+    ))
+  }
   return function(req, res, next) {
+    if (options.method && options.method != req.method) return next()
     req.parsedUrl = req.parsedUrl || url.parse(req.url)
     var path = req.parsedUrl.pathname
-    // use constructor because instanceof fails between modules
-    if (route.constructor.name != 'RegExp') {
-      route = new RegExp('^' +
-        String(route)
-        .replace(specialChars, '\\$&')
-        .replace(/\*\*/g, '(.+)')
-        .replace(/\*/g, '([^/]+)') +
-        '$'
-      )
-    }
     if (!route.exec(path)) return next()
     cb(req, res, next)
   }
 }
 
-module.exports = rut
-module.exports.all = rut
+module.exports = module.exports.all = rut
+
+var methods = ['GET', 'POST', 'DELETE']
+methods.forEach(function(method) {
+  module.exports[method.toLowerCase()] = function(route, options, cb) {
+    if (!cb) {
+      cb = options
+      options = {}
+    }
+    options.method = method
+    return rut(route, options, cb)
+  }
+})
